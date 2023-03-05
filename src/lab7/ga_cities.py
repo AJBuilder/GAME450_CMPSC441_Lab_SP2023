1a282d58c47b00fab03f25be2a8c77a7cfc6e49b
AJBuilder <adam.aj.robertson@gmail.com>
2023-03-04 23:59:01 -0500

    Lab7 fin, added cubic to closest fitness

--- src/lab7/ga_cities.py
+++ src/lab7/ga_cities.py
@@ -58,14 +58,28 @@ def game_fitness(cities, idx, elevation, size):
     ############### City distance fitness ###############
     
     # Here we have to consider three attributes
-    # - Being too close to another city
-    # - Clustering together in different clusters
-    # - Not clustering around the center of the map
-    # While looping through the city locations,
-    # First, find the non-euclidian, x/y distances between a city and all other cities
-    #average_distances=[]
+    # 1: Clustering together unevenly
+    # 2: Not clustering around the center of the map
+    # 3: Being too close to another city
+    # 
+    # We can solve these by checking two things:
+    #
+    # First, ideally the average distance between one city and all the other cities is
+    # the same as the distance from that one city to the center of the map.
+    # This will not only naturally space the cities out symetrically around the map, but 
+    # will ensure the cities congregate around the center of the map. This solves 1 and 2.
+    #
+    # Second, we need to make sure cities aren't too close to each other.
+    # To do this we just check the closest city to make sure it is not too close.
+    # We don't really care about the closest city being too far away. This solves 3.
+    
     clustering_distance_fitnesses = []
     closest_distance_fitnesses=[]
+    
+    # When comparing city differences, we want this function to scale with the map size
+    # So we define the "max" as the distance from the center to a corner,
+    # and the "min" as the distance between each city if they each had their own sector
+    # and were evenly spaced.
     max_distance = math.sqrt( size[0]**2 + size[1]**2)
     min_distance = math.sqrt((size[0] / (len(cities)))**2 + (size[0] / (len(cities)))**2)
     for i,loc in enumerate(locations):
@@ -98,33 +112,30 @@ def game_fitness(cities, idx, elevation, size):
         cluster_divider = 20 # The higher, the less variation in the cluster.
         clustering_distance_fitnesses.append(math.exp(-(error/(max_distance/cluster_divider))**2))
         
-        # For closest city/distance fitness, the closest a city can be is zero, 
-        # and the farthest a city can be is 1.
-        # We care more about the closest city being too close than too far.
-        fitness = (smallest_dist-min_distance)/(max_distance-min_distance) # (x-low)/(high/low)
+        # For closest city/distance fitness, a cubic funtion is used.
+        # This makes everything equal or closer than the minimum distance a 0 (when floored to 0)
+        # and really punishes anything close to the minimum until it gets closer to the "max"
+        # We care more about the closest city being too close than too far. 
+        # Experimentally, a linear function doesn't punish close cities enough.
+        # It looks similar to:
+        #                _____
+        #               |
+        #               |
+        #             _|
+        #       _____/
+        #______/
+        # Where x is the closest city distance.
+        fitness = (smallest_dist-min_distance)**3/(max_distance-min_distance) # (x-low)^3/(high/low)
         if fitness < 0:
             fitness = 0
+        if fitness > 1:
+            fitness = 1
             
         closest_distance_fitnesses.append(fitness)
             
                     
-    # Ideally, the average distance between one city and all the other cities is
-    # the same as the distance to the center of the map.
-    # This will not only naturally space the cities out, but will ensure the cities congregate around
-    # the center of the map.
-    #ideal_distances = []
-    #for loc in locations:
-    #    ideal_distances.append(( (size[0]/2) - loc[0], (size[1]/2) - loc[1] ))
-    
-    # Find how each distance compares to the ideal distance.
-    # Exponential function punishes cities that are really far away from the ideal distance
-    #distance_fitnesses = []
-    #for avg, ideal in zip(average_distances, ideal_distances):
-    #    diff = math.sqrt( (ideal[0] - avg[0])**2 + (ideal[1] - avg[1])**2)
-    #    distance_fitnesses.append(math.exp(-(diff/(max_distance/6))**2)) # e^(-(x/d)^4) where d is the x-value
-                                                                        # that should return about .5.
-        
-    
+   
+   
     ############### Padding fitness ###############
         
     # The map looks better if the cities aren't super close to the map border
@@ -139,6 +150,9 @@ def game_fitness(cities, idx, elevation, size):
         fitness =  1 - (math.sqrt(dist[0]**2 + dist[1]**2) / maxDistance)**5
         padding_fitnesses.append(fitness)  
                                                                                                 
+    # The exponent of 5 makes it so we really only punish fitness
+    # when the city is really, really close.
+    
     
     #### Final Fitness ####
     
@@ -154,7 +168,7 @@ def game_fitness(cities, idx, elevation, size):
     average_clustering_fitness = movingWeightedAverage(sorted(clustering_distance_fitnesses))
     average_closest_fitness = movingWeightedAverage(sorted(closest_distance_fitnesses))
     average_padding_fitness = movingWeightedAverage(sorted(padding_fitnesses))
-    average_fitness = (average_elevation_fitness + average_clustering_fitness + average_padding_fitness + average_closest_fitness) / 4
+    average_fitness = (average_elevation_fitness + average_clustering_fitness + average_closest_fitness + average_padding_fitness) / 4
 
     
     
